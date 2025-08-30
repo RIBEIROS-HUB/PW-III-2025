4253814e75fc998580bb8cc9bdc15cc33d1e7d52
RIBEIROS-HUB <daniel.santana64@etec.sp.gov.br>
2025-08-29 22:34:31 -0300

    apiBomPreco2

--- apiBomPreco/routes/api.php
+++ apiBomPreco/routes/api.php
@@ -1,8 +1,20 @@
 <?php
-
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//rotas para visualizar os registros criados nas tabelas
+Route::get('/', function(){return response()->json(['Sucesso'=>true]);});
+Route::get('/produto', [ProdutosController::class,'index']);
+Route::get('/produtos/{codigo}', [ProdutosController::class,'show']);
+
+//rota para inserir os registros na tabela
+Route::post('/produtos', [ProdutosController::class,'store']);
+
+//rota para alterar os registros da tabela
+Route::put('/produtos/{codigo}', [ProdutosController::class,'update']);
+
+//rota para excluir o registro por id/codigo
+Route::delete('/produtos/{id}', [ProdutosController::class,'destroy']);
+
+
